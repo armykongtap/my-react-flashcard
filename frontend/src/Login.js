b8d4a6775639354708b08d644c84fd6ced7ca7b9
armykongtap <35949144+armykongtap@users.noreply.github.com>
2019-11-07 12:27:34 +0700

    build Login page

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,8 +1,50 @@
 import React from "react";
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: "", password: "" };
+
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeUsername(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  handleChangePassword(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(this.state.username + "   " + this.state.password);
+    event.preventDefault();
+  }
+
   render() {
-    return <h1>Login</h1>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Username :
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={this.handleChangeUsername}
+          />
+          <br />
+          Password :
+          <input
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChangePassword}
+          />
+        </label>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
 
