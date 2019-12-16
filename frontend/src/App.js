dcaaeda365cf0a69088b7f036141a94fcfc9b12b
armykongtap <35949144+armykongtap@users.noreply.github.com>
2019-12-16 13:22:51 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,8 +31,6 @@ class App extends React.Component {
 
             {isGUEST ? null : <Link to="/logout">Logout</Link>}
           </div>
-          {/* A <Switch> looks through its children <Route>s and
-              renders the first one that matches the current URL. */}
           <Switch>
             <Route path="/register">
               <Register />
